aa70468b41091db6e84d326133901f225c776c96
Matthew Baxendale <mdbaxendale1@gmail.com>
2022-06-18 15:49:48 +0100

    broker-service: initial router config, docker-file & docker-compose config

---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+
+	mux := chi.NewRouter()
+
+	// Basic CORS
+	mux.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(mux *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	//middleware
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/", app.Broker)
+
+	return mux
+
+}
